6bc6b0cd3780a222b61a9100c9a89085acfd306c
Alexpux <alexey.pawlow@gmail.com>
2015-06-23 11:57:07 +0300

    mod_loader: mingw always link to msvcrt.dll

--- src/jrd/os/win32/mod_loader.cpp
+++ src/jrd/os/win32/mod_loader.cpp
@@ -74,7 +74,7 @@ public:
 // if we don't use MSVC then we don't use MS CRT ?
 // NS: versions of MSVC before 2005 and, as preliminary reports suggest,
 // after 2008 do not need this hack
-#if !defined(_MSC_VER) || (_MSC_VER < 1400)
+#if (!defined(_MSC_VER) || (_MSC_VER < 1400)) && !defined(__MINGW32__)
 		return;
 #else
 
@@ -89,7 +89,9 @@ public:
 		if ((*mFindActCtxSectionString)
 				(0, NULL,
 				ACTIVATION_CONTEXT_SECTION_DLL_REDIRECTION,
-#if _MSC_VER == 1400
+#if defined(__MINGW32__)
+                    "msvcrt.dll",
+#elif _MSC_VER == 1400
                     "msvcr80.dll",
 #elif _MSC_VER == 1500
                     "msvcr90.dll",
